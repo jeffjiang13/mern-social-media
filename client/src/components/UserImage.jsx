a2db7688c07c8f2cfa3709e07ae445c8c89d4449
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-07 14:08:42 -0500

    fixing image

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -3,13 +3,15 @@ import { Box } from "@mui/material";
 const UserImage = ({ image, size = "60px" }) => {
   return (
     <Box width={size} height={size}>
-      <img
-        style={{ objectFit: "cover", borderRadius: "50%" }}
-        width={size}
-        height={size}
-        alt="user"
-        src={`https://mern-social-media-v9m2.onrender.com/assets/${image}`}
-      />
+      {image && (
+        <img
+          style={{ objectFit: "cover", borderRadius: "50%" }}
+          width={size}
+          height={size}
+          alt="user"
+          src={`https://mern-social-media-v9m2.onrender.com/assets/${image}`}
+        />
+      )}
     </Box>
   );
 };
